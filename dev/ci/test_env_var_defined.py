444bb97a33bb581416fe7a467256465127ee781d
antoine <antoine@thinkr.fr>
2024-04-10 09:55:01 +0000

    (ci): use vars context instead of env
    - To retrieve variable define in github repository. env context is made
      to retrieve variables defined directly within the worflow yaml.
    - Remove python workflow experiment

--- dev/ci/test_env_var_defined.py
+++
@@ -1,9 +0,0 @@
-import os
-if os.environ.get('ENVVAR') is None:
-  print('ENVVAR is defined')
-else:
-  print('ENVVAR is not defined')
-if os.environ.get('SECRET') is None:
-  print('SECRET is defined')
-else:
-  print('SECRET is not defined')
